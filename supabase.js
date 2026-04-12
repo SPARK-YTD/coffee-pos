f325442b3397102e4fb0549c2179e39d86dbb6f1
SPARK-YTD <67884933+SPARK-YTD@users.noreply.github.com>
2026-04-12 19:00:33 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-const SUPABASE_URL = "https://olbimevfyvnzvbemkjcl.supabase.co";
-const SUPABASE_KEY = "sb_publishable_95CPRjLDy...";
+const SUPABASE_URL = "https://olbimevfyvnvzbemkjcl.supabase.co";
+const SUPABASE_KEY = "sb_publishable_95CPRjLDyzhrqUG7UgouzA_eqM2f9J1";
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
