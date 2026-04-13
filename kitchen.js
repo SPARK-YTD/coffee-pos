d77b6b1ee0cfa04156065471fad8bf56d707aca4
SPARK-YTD <67884933+SPARK-YTD@users.noreply.github.com>
2026-04-13 13:26:19 +0300

    Update kitchen.js

--- kitchen.js
+++ kitchen.js
@@ -1,5 +1,5 @@
 async function loadOrders() {
-  const { data, error } = await supabaseClient
+  const { data: orders, error } = await supabaseClient
     .from("orders")
     .select("*")
     .eq("status", "pending")
@@ -13,20 +13,33 @@ async function loadOrders() {
   const container = document.getElementById("orders");
   container.innerHTML = "";
 
-  data.forEach(order => {
+  for (let order of orders) {
+    // نجيب عناصر الطلب
+    const { data: items } = await supabaseClient
+      .from("order_items")
+      .select("*")
+      .eq("order_id", order.id);
+
     const div = document.createElement("div");
-    div.style.border = "1px solid black";
+    div.style.border = "2px solid black";
     div.style.margin = "10px";
     div.style.padding = "10px";
 
+    let itemsHTML = "";
+
+    items.forEach(item => {
+      itemsHTML += `<li>${item.item_name} - ${item.price}</li>`;
+    });
+
     div.innerHTML = `
       <h3>طلب #${order.id.slice(0, 5)}</h3>
+      <ul>${itemsHTML}</ul>
       <p>المجموع: ${order.total} BD</p>
       <button onclick="markReady('${order.id}')">جاهز</button>
     `;
 
     container.appendChild(div);
-  });
+  }
 }
 
 async function markReady(orderId) {
