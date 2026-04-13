4f08214414ddd25c80cc871d4cece3913a8b934a
SPARK-YTD <67884933+SPARK-YTD@users.noreply.github.com>
2026-04-13 11:49:17 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-const supabaseUrl = "https://olbimevfyvnvzbemkjcl.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9sYmltZXZmeXZudnpiZW1ramNsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU5MTQ4MDUsImV4cCI6MjA5MTQ5MDgwNX0.c9ilX5NbI6T7DW99G-6ifp1dbBK3T0YVEHlNb-QEci0";
+const supabaseUrl = "https://kjwdhusevgrbspwqsvsm.supabase.co";
+const supabaseKey = "sb_publishable_dcejME7Sld3Sy9P8c7wa9Q_tvJ7tEnA";
 
-window.supabaseClient = window.supabase.createClient(supabaseUrl, supabaseKey);
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
