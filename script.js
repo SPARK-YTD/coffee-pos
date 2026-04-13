2714db54b9f572fd86f5af08d26abe8b5b1f8398
SPARK-YTD <67884933+SPARK-YTD@users.noreply.github.com>
2026-04-13 12:36:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
+let cart = [];
+let total = 0;
 
+function addItem(name, price) {
+  cart.push({ name, price });
+  total += price;
+
+  updateCart();
+}
+
+function updateCart() {
+  const cartList = document.getElementById("cart");
+  cartList.innerHTML = "";
+
+  cart.forEach(item => {
+    const li = document.createElement("li");
+    li.textContent = `${item.name} - ${item.price}`;
+    cartList.appendChild(li);
+  });
+
+  document.getElementById("total").textContent = "المجموع: " + total.toFixed(2);
+}
+
+function checkout() {
+  alert("تم الطلب! 💸");
+
+  cart = [];
+  total = 0;
+  updateCart();
+}
