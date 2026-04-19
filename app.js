3de27f176703f48ff48815bac647ac7cec5f82a9
SPARK-YTD <67884933+SPARK-YTD@users.noreply.github.com>
2026-04-20 00:02:07 +0300

    Update app.js

--- app.js
+++ app.js
@@ -20,10 +20,10 @@ async function openShiftPrompt() {
 }
 
   const { data: emp } = await supabase
-    .from("employees")
-    .select("*")
-    .eq("pin", pin)
-    .single();
+  .from("employees")
+  .select("*")
+  .eq("pin", pin)
+  .maybeSingle();
 
 if (!emp) {
   alert("❌ PIN خطأ");
