67e162f88a9f4f2a797e14ecd59f90ddea38ea0c
SPARK-YTD <67884933+SPARK-YTD@users.noreply.github.com>
2026-05-06 22:37:41 +0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,8 @@
-import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 export const supabase = createClient(
   "https://kjwdhusevgrbspwqsvsm.supabase.co",
   "sb_publishable_dcejME7Sld3Sy9P8c7wa9Q_tvJ7tEnA"
 );
+
+window.supabase = supabase;
